cf53339c8e71f209dc6eb7049865cf6ff98961dd
Mateus Pereira <mateus.sle.per@hotmail.com>
2023-04-24 11:20:06 -0300

    Estrutura final

---
+++ Código/Node.py
@@ -0,0 +1,8 @@
+# Mateus Slezinsky Pereira - 21200422
+# Augusto Teixeira da Silva - 22102556
+
+class Node:
+    def __init__(self, dados=None):
+        self.dados = dados
+        self.anterior = None
+        self.proximo = None
